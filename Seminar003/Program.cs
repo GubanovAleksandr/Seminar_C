7c1212e6a55cf0e6646cea75518681054a3e576a
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-22 17:04:08 +0500

    Задача 3 решена

--- Seminar003/Program.cs
+++ Seminar003/Program.cs
@@ -1,5 +1,12 @@
 ﻿Console.WriteLine("Введите первое число");
 int number1 = Convert.ToInt32(Console.ReadLine());
-COnsole.WriteLine("Введите второе число");
+Console.WriteLine("Введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1)
+if (number1 == number2*number2)
+{
+    Console.WriteLine($"{number1} является квадратом второго");
+}
+else
+{
+    Console.WriteLine($"{number2} не является квадратом первого числа ");
+}

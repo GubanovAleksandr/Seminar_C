d55c62ece594f7a1f6ece6dab4cbbd82ccf78ffe
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-22 16:53:26 +0500

    autofix

--- Seminar003/Program.cs
+++ Seminar003/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+COnsole.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1)
